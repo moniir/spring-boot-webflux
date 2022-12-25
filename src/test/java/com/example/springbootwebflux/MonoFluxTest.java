035ca5c7011eef2dee770c05850a6ef90995afee
Monir Hossain <monir198@gmail.com>
2022-12-25 12:57:31 +0600

    Added flux example in test file

--- src/test/java/com/example/springbootwebflux/MonoFluxTest.java
+++ src/test/java/com/example/springbootwebflux/MonoFluxTest.java
@@ -1,6 +1,7 @@
 package com.example.springbootwebflux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
@@ -9,5 +10,17 @@ public class MonoFluxTest {
     public void testMono(){
         Mono<String> stringMono = Mono.just("Monir").log();
         stringMono.subscribe(System.out::println);
+    }
+    @Test
+    public void testMonoWithException(){
+        Mono<?> stringMono = Mono.just("Monir")
+                .then(Mono.error(new RuntimeException("Unusual exception occured"))).log();
+        stringMono.subscribe(System.out::println, (e)-> System.out.println(e.getMessage()));
+    }
+    @Test
+    public void testFlux(){
+        Flux<String> stringFlux = Flux.just("Monir","Hossain","Bhuiyan")
+                .concatWithValues("Java").log();
+        stringFlux.subscribe(System.out::println);
     }
 }
